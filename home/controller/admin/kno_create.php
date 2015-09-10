bdb704db0e4c8df3bc04117a9b525489de8a78a5
huzhiming12 <hzm4225060@163.com>
2015-09-10 16:05:30 +0800

    Signed-off-by: huzhiming12 <hzm4225060@163.com>

---
+++ home/controller/admin/kno_create.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huzhiming
+ * Date: 2015/9/10
+ * Time: 16:00
+ */
+
+require_once "../../smarty_include.php";
+
+$smarty->display("admin/kno_create.html");
